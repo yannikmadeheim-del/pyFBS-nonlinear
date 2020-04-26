3d0d7da85d4f15a2a82430243c2fcd0a4c4ccda1
Tomaž Bregar <bregar.toma@gmail.com>
2020-04-26 15:47:39 +0200

    Sensor rotation around local CSY
    Former-commit-id: 9c95536f1b89f94d95ab953bf73bccf7e2db2ce8 [formerly 4b1b6d32c1a18009e570161474af9944e0766752]
    Former-commit-id: edff73e2c57658dcdcf3dcfd12b019f985a21d07

--- pyFBS/display_interactive.py
+++ pyFBS/display_interactive.py
@@ -1,49 +1,54 @@
 import pyvista as pv
 import numpy as np
-import numpy as np
-
-points = np.array([[0, 0, 0],
-                   [0, 0.5, 0.5],
-                   [0.5, 0, 0.5],
-                   [0.5, 0.5, 0]])
 
 
-def unit_vector(vector):
-    return vector / np.linalg.norm(vector)
+from numpy import cross, eye, dot
+from scipy.linalg import expm, norm
 
-
-def angle_between(v1, v2):
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-
+def M(axis, theta):
+    """
+    Euler-Rodrigues formula
+    """
+    return expm(cross(eye(3), axis/norm(axis)*theta))
 
 class Accelerometer():
     def __init__(self, p, N):
 
         # accelerometer
         self.box = pv.Box()
-        self.box.translate([1, 1, 1])
+        #self.box.translate([1, 1, 1])
         self.box.points /= 2
 
         p.add_mesh(self.box, opacity=0.3, show_edges=True, color="#8c8c8c")
 
-        ray_x = pv.Line([0, 0, 0], [1, 0, 0])
+        ray_x = pv.Line([-0.5, -0.5, -0.5], [0.5, -0.5, -0.5])
         p.add_mesh(ray_x, color="r", line_width=3)
-        ray_y = pv.Line([0, 0, 0], [0, 1, 0])
+        ray_y = pv.Line([-0.5, -0.5, -0.5], [-0.5, 0.5, -0.5])
         p.add_mesh(ray_y, color="g", line_width=3)
-        ray_z = pv.Line([0, 0, 0], [0, 0, 1])
+        ray_z = pv.Line([-0.5, -0.5, -0.5], [-0.5, -0.5, 0.5])
         p.add_mesh(ray_z, color="b", line_width=3)
 
         self.accelerometer = [self.box, ray_x, ray_y, ray_z]
         self.N = int(N * 4)
 
-    def callback(self, point, i):
-        print(point,i)
-        # 3D translation in space
-        if i == 0:
+        self.local_orientation = np.asarray([[1, 0, 0],
+                          [0, 1, 0],
+                          [0, 0, 1]])
 
-            _new = point - self.box.center_of_mass() + [0.5, 0.5, 0.5]
+        self.local_widgets = np.array([[0, 0, 0],
+                   [-.5, 0.2, 0.2],
+                   [0.2, -.5, 0.2],
+                   [0.2, 0.2, -.5]])
+
+        self.def_rot = 0
+
+
+
+    def callback(self, point, i):
+        #print(point,i)
+
+        if i == 0:
+            _new = point - self.box.center_of_mass()
 
             for item in self.accelerometer:
                 item.translate(_new)
@@ -52,26 +57,35 @@ class Accelerometer():
                 p.sphere_widgets[self.N + 1 + i].SetCenter(
                     _new + np.asarray(p.sphere_widgets[self.N + 1 + i].GetCenter()))
 
+        # 3D rotation
         else:
-            _new = self.box.center_of_mass() - [0.5, 0.5, 0.5]
+            # get the center of acc
+            _new = self.box.center_of_mass()
 
-            _vec1 = np.asarray(point - _new)
-            _vec2 = np.asarray(points[i, :])
+            # define the rotational matrix based on angle of rotation
+            # currently self.def_rot is fixed and is not depended on the position of sphere_widget
+            rot = M(self.local_orientation[i-1, :], self.def_rot * np.pi / 180)
 
-            #if i == 3:
-            #    print(_vec1[[0, 1]])
-            #    print(i, angle_between(_vec1[[0, 1]], _vec2[[0, 1]]) * 180 / np.pi)
+            # rotate everything within accelerometer
+            for item in self.accelerometer:
+                item.points = (rot @ (item.points - _new).T).T + _new
 
-            for k in range(3):
-                p.sphere_widgets[self.N + 1 + k].SetCenter(_new + points[1 + k, :])
+            # orient the local csys of accelerometer with the new rotation
+            self.local_orientation = (rot @ (self.local_orientation).T).T
+
+            # position all widgets to the new position
+            for k in range(4):
+                self.local_widgets[k, :] = rot@self.local_widgets[k, :]
+                p.sphere_widgets[self.N +  k].SetCenter(self.local_widgets[k, :]  + _new)
+
 
     def translate(self, point):
-        _new = point - self.box.center_of_mass() + [0.5, 0.5, 0.5]
+        _new = point - self.box.center_of_mass()
         for item in self.accelerometer:
             item.translate(_new)
 
-        for i in range(4):
-            p.sphere_widgets[self.N + i].SetCenter(_new + np.asarray(p.sphere_widgets[self.N + i].GetCenter()))
+        for k in range(4):
+            p.sphere_widgets[self.N + k].SetCenter(self.local_widgets[k, :] + point)
 
 
 if __name__ == '__main__':
@@ -79,9 +93,27 @@ if __name__ == '__main__':
     p = pv.Plotter()
     p.background_color = "#D4D4D4"
 
-    for i in range(20):
+    size = 0.5
+    arrow = pv.Arrow(start=(0.0, 0.0, 0.0), direction=(1, 0, 0))
+    arrow.points *= size
+    p.add_mesh(arrow, color="r")
+
+    arrow = pv.Arrow(start=(0.0, 0.0, 0.0), direction=(0, 1, 0))
+    arrow.points *= size
+    p.add_mesh(arrow, color="g")
+
+    arrow = pv.Arrow(start=(0.0, 0.0, 0.0), direction=(0, 0, 1))
+    arrow.points *= size
+    p.add_mesh(arrow, color="b")
+
+    for i in range(3):
         _gg = Accelerometer(p, i)
-        p.add_sphere_widget(_gg.callback, center=points, color=["k", "r", "g", "b"], radius=0.05)
+        p.add_sphere_widget(_gg.callback, center=_gg.local_widgets, color=["k", "r", "g", "b"], radius=0.05)
         _gg.translate(np.random.random(3) * 10)
+
+        _gg.def_rot = 10
+
+    p.reset_camera()
+
 
     p.show()
