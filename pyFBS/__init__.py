4b6650cdee8d835654c930f230902871f878d73a
Tomaž Bregar <bregar.toma@gmail.com>
2020-05-18 14:42:25 +0200

    init variable + utility clear
    Former-commit-id: 023aeb874cf9bfec9461dffb6b4a1317a49195c4 [formerly a9a54193a472336a9188ff24089968123f046fab]
    Former-commit-id: 45f6d58104be61ef72dd3996c92822a2bd52bfae

--- pyFBS/__init__.py
+++ pyFBS/__init__.py
@@ -4,3 +4,9 @@ from .VPT import *
 from .display import *
 from .SEMM import *
 from .MCK import *
+
+
+from pathlib import Path
+import os 
+
+engine_mount = str(Path(__file__).parents[1] )+os.sep+"data" +os.sep + "AM_automotive_testbench" + os.sep + "STL" + os.sep + "engine_mount.stl"
