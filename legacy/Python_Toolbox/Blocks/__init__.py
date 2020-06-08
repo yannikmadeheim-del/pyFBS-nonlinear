5a4e4a5cad60fa87bbc552866f2bb5a4e08c66d8
Tomaž Bregar <bregar.toma@gmail.com>
2020-06-08 06:59:04 +0200

    Removed legacy code
    Former-commit-id: 16abbce0a67db7df8d9a6368d909b734c5bcbe42 [formerly eb7a78b2b80216cc925951ec148d58eb726e1193]
    Former-commit-id: f8fcd3e923d7d5785740b3ca1a9551f513934e93

--- legacy/Python_Toolbox/Blocks/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 30 16:46:51 2018
-
-@author: Umer Sherdil Paracha
-"""
-
-from .Blocks import Blocks
-from .Create_Block_List import create_block_list
--- legacy/Python_Toolbox/Time_Blocks/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 30 16:46:51 2018
-
-@author: Umer Sherdil Paracha
-"""
-
-from .From_Time_Series import from_time_series
-from .Time_Blocks import Time_Blocks
