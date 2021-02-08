3aec129086d39eb52f4d8e8ada5295040c35a60d
Tomaž Bregar <bregar.toma@gmail.com>
2021-02-08 17:34:12 +0100

    Integrate into MCK

--- pyFBS/MCK.py
+++ pyFBS/MCK.py
@@ -1,7 +1,9 @@
 from scipy.sparse import linalg,diags
 import pyansys
 from numpy.random import randn
+from pyFBS.VPT import VPT
 
+import pandas as pd
 import scipy as sp
 import numpy as np
 from scipy import spatial
@@ -352,42 +354,44 @@ class MK_model(object):
         self.freq = freq
 
 
-    def full_DoF_FRF_synth(nodes, df_imp, df_sen,f_start = 1, f_end = 2000, f_resolution= 1, limit_modes = None, modal_damping = None, frf_type = "receptance",_all = False):
+    def full_DoF_FRF_synth(self, df_imp, df_sen,f_start = 1, f_end = 2000, f_resolution= 1, limit_modes = None, modal_damping = None, frf_type = "receptance", _all = False):
     
         imp_coord = np.asarray([df_imp['Position_1'], df_imp['Position_2'], df_imp['Position_3']]).T
         sen_coord = np.asarray([df_sen['Position_1'], df_sen['Position_2'], df_sen['Position_3']]).T
         
         # finding three nearest nodes
         ind_imp = np.zeros_like(imp_coord,dtype=int)
-        nodes_copy = np.copy(nodes)
+        nodes_copy = np.copy(self.nodes)
+
         for i in range(ind_imp.shape[1]):
-            ind_imp[:,i] = MK.find_nearest_locations(nodes_copy, imp_coord)
+            ind_imp[:,i] = self.find_nearest_locations(nodes_copy, imp_coord)
             nodes_copy[ind_imp[:,i]] = 0 # change nearest node to 0 not to be selected in next loops
         ind_sen = np.zeros_like(sen_coord,dtype=int)
-        nodes_copy = np.copy(nodes)
+
+        nodes_copy = np.copy(self.nodes)
         for j in range(ind_sen.shape[1]):
-            ind_sen[:,j] = MK.find_nearest_locations(nodes_copy, sen_coord)
+            ind_sen[:,j] = self.find_nearest_locations(nodes_copy, sen_coord)
             nodes_copy[ind_sen[:,j]] = 0 # change nearest node to 0 not to be selected in next loops
             
         #generating data frame for impacts
         df_imp_ = np.zeros((int(3*3*ind_imp.shape[0]),3)) # assume nine nearest impacts for VPT
-        for k in range(nodes[ind_imp].shape[0]):
-            df_imp_[9*k:9+9*k,:] = np.repeat(np.asarray([[nodes[ind_imp][k,:]]][0]),3,axis=1) # assume nine nearest impacts for VPT
+        for k in range(self.nodes[ind_imp].shape[0]):
+            df_imp_[9*k:9+9*k,:] = np.repeat(np.asarray([[self.nodes[ind_imp][k,:]]][0]),3,axis=1) # assume nine nearest impacts for VPT
         df_imp = pd.DataFrame(data=df_imp_,columns=('Position_1','Position_2','Position_3'))
-        df_imp['Direction_1'] = np.tile([1,0,0,1,0,0,1,0,0],nodes[ind_imp].shape[0])
-        df_imp['Direction_2'] = np.tile([0,1,0,0,1,0,0,1,0],nodes[ind_imp].shape[0])
-        df_imp['Direction_3'] = np.tile([0,0,1,0,0,1,0,0,1],nodes[ind_imp].shape[0])
+        df_imp['Direction_1'] = np.tile([1,0,0,1,0,0,1,0,0],self.nodes[ind_imp].shape[0])
+        df_imp['Direction_2'] = np.tile([0,1,0,0,1,0,0,1,0],self.nodes[ind_imp].shape[0])
+        df_imp['Direction_3'] = np.tile([0,0,1,0,0,1,0,0,1],self.nodes[ind_imp].shape[0])
         df_imp['Grouping'] = np.repeat([np.arange(ind_imp.shape[0])], 9)
         df_imp['Quantity'] = np.tile(np.repeat(['Acceleration'], 9), ind_imp.shape[0])
             
         #generating data frame for channels
         df_chn_ = np.zeros((int(3*3*ind_sen.shape[0]),3)) # assume three nearest sensors (9 channels) for VPT
-        for l in range(nodes[ind_sen].shape[0]):
-            df_chn_[9*l:9+9*l,:] = np.repeat(np.asarray([[nodes[ind_sen][l,:]]][0]),3,axis=1) # assume three nearest sensors for VPT
+        for l in range(self.nodes[ind_sen].shape[0]):
+            df_chn_[9*l:9+9*l,:] = np.repeat(np.asarray([[self.nodes[ind_sen][l,:]]][0]),3,axis=1) # assume three nearest sensors for VPT
         df_chn = pd.DataFrame(data=df_chn_,columns=('Position_1','Position_2','Position_3'))
-        df_chn['Direction_1'] = np.tile([1,0,0,1,0,0,1,0,0],nodes[ind_sen].shape[0])
-        df_chn['Direction_2'] = np.tile([0,1,0,0,1,0,0,1,0],nodes[ind_sen].shape[0])
-        df_chn['Direction_3'] = np.tile([0,0,1,0,0,1,0,0,1],nodes[ind_sen].shape[0])
+        df_chn['Direction_1'] = np.tile([1,0,0,1,0,0,1,0,0],self.nodes[ind_sen].shape[0])
+        df_chn['Direction_2'] = np.tile([0,1,0,0,1,0,0,1,0],self.nodes[ind_sen].shape[0])
+        df_chn['Direction_3'] = np.tile([0,0,1,0,0,1,0,0,1],self.nodes[ind_sen].shape[0])
         df_chn['Grouping'] = np.repeat([np.arange(ind_sen.shape[0])], 9)
         df_chn['Quantity'] = np.tile(np.repeat(['Acceleration'], 9), ind_sen.shape[0])
         
@@ -403,7 +407,9 @@ class MK_model(object):
         df_vp_imp['Direction_2'] = np.tile([0,1,0,0,1,0],imp_coord.shape[0])
         df_vp_imp['Direction_3'] = np.tile([0,0,1,0,0,1],imp_coord.shape[0])
         df_vp_imp['Quantity'] = np.tile(np.repeat(['Acceleration', 'Rotational Acceleration'], 3), imp_coord.shape[0])
-        df_vp_imp['Grouping'] = np.repeat([np.arange(imp_coord.shape[0])], imp_coord.shape[0])
+        #df_vp_imp['Grouping'] = np.repeat([np.arange(imp_coord.shape[0])], imp_coord.shape[0])
+        df_vp_imp['Grouping'] = np.repeat([np.arange(imp_coord.shape[0])], 6)
+
         df_vp_imp['Description'] = np.tile(['fx','fy','fz','mx','my','mz'],imp_coord.shape[0])
         
         # generating data frame for channel virtual points
@@ -415,11 +421,12 @@ class MK_model(object):
         df_vp_chn['Direction_2'] = np.tile([0,1,0,0,1,0],sen_coord.shape[0])
         df_vp_chn['Direction_3'] = np.tile([0,0,1,0,0,1],sen_coord.shape[0])
         df_vp_chn['Quantity'] = np.tile(np.repeat(['Acceleration', 'Rotational Acceleration'], 3), sen_coord.shape[0])
-        df_vp_chn['Grouping'] = np.repeat([np.arange(imp_coord.shape[0])], sen_coord.shape[0])
+        #df_vp_chn['Grouping'] = np.repeat([np.arange(imp_coord.shape[0])], sen_coord.shape[0])
+        df_vp_chn['Grouping'] = np.repeat([np.arange(imp_coord.shape[0])], 6)
         df_vp_chn['Description'] = np.tile(['ux','uy','uz','tx','ty','tz'],sen_coord.shape[0])
         
         # empty array
-        FRF_FDoF = np.zeros((MK.FRF.shape[0],ind_sen.shape[0]*6,ind_imp.shape[0]*6),dtype=complex)
+        FRF_FDoF = np.zeros((self.FRF.shape[0],ind_sen.shape[0]*6,ind_imp.shape[0]*6),dtype=complex)
         
         # apply VPT
         for res_ in df_chn['Grouping'].unique():
@@ -428,13 +435,16 @@ class MK_model(object):
                 _df_imp = df_imp[df_imp['Grouping'] == exc_]
                 _df_vp_imp = df_vp_imp[df_vp_imp['Grouping'] == exc_]
                 # Set impacts Group to match responses Group
-                _df_imp['Grouping'] = res_
-                _df_vp_imp['Grouping'] = res_
+                #print("a", _df_imp['Grouping'], _df_vp_imp['Grouping'])
+                #_df_imp['Grouping'] = res_
+                #_df_vp_imp['Grouping'] = res_
+                #print("b",_df_imp['Grouping'], _df_vp_imp['Grouping'])
+
                 # Read responses and VP responses
                 _df_chn = df_chn[df_chn['Grouping'] == res_]
                 _df_vp_chn = df_vp_chn[df_vp_chn['Grouping'] == res_]
                 vpt_ = VPT(_df_chn, _df_imp, _df_vp_chn, _df_vp_imp)
-                vpt_.apply_VPT(MK.freq, MK.FRF[:,9*res_:9*res_+9,9*exc_:9*exc_+9])
+                vpt_.apply_VPT(self.freq, self.FRF[:,9*res_:9*res_+9,9*exc_:9*exc_+9])
                 FRF_FDoF[:,6*res_:6*res_+6,6*exc_:6*exc_+6] = vpt_.vptData            
         
         return FRF_FDoF
