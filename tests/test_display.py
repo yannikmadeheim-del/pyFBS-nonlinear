e9171820a6b83099b06b55a0d9cb8a57e6d39309
Tomaž Bregar <bregar.toma@gmail.com>
2021-06-27 15:15:51 +0200

    Fixed error

--- tests/test_display.py
+++ tests/test_display.py
@@ -8,8 +8,8 @@ from pyvistaqt import BackgroundPlotter, MainWindow, QtInteractor
 #can't test with showing axes - segmentation fault on travis
 #@pytest.mark.parametrize("show_axes", [False, True])
 @pytest.mark.parametrize("show_origin", [False, True])
-def test_display(show_origin,show_axes):
-    view3D = pyFBS.view3D(show_origin = show_origin,show_axes = show_axes, title = "test")
+def test_display(show_origin):
+    view3D = pyFBS.view3D(show_origin = show_origin,show_axes = False, title = "test")
     assert(view3D.plot is not None)
     view3D.plot.close()
 
