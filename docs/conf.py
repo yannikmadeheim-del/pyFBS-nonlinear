bf38698f9e9b758613b3bdb1907ea6177b2d3c21
Tomaž Bregar <bregar.toma@gmail.com>
2021-07-13 07:45:05 +0200

    Alternative theme

--- docs/conf.py
+++ docs/conf.py
@@ -111,8 +111,11 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 #html_theme = 'alabaster'
+
+import pydata_sphinx_theme
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -153,8 +156,8 @@ latex_elements = {
 }
 
 
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#import sphinx_rtd_theme
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_context = {
     # Enable the "Edit in GitHub link within the header of each page.
