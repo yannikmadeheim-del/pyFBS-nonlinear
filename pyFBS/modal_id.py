9450095a0e3e99e492fccd620ec70be1f754b50d
Miha Kodrič <kodric94miha@gmail.com>
2022-06-03 10:39:33 +0000

    Another fix

--- pyFBS/modal_id.py
+++ pyFBS/modal_id.py
@@ -169,7 +169,7 @@ class modal_id(object):
             Y_ = np.block([[[self.FRF.real]],[[self.FRF.imag]]])
             A_ = np.linalg.lstsq(P.transpose(1,0,2).reshape(-1, P.shape[-1]),
                                  Y_.transpose(2,0,1).reshape(-1, Y_.shape[-2]))
-            Ar, Ai = np.split(A_[:2*s.shape[0]], 2) 
+            Ar, Ai = np.split(A_[:2*s.shape[1]], 2) 
             A = (Ar + 1.j*Ai).T
 
             self.A = A
