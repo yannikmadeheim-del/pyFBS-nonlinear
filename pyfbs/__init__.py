7cfe63947d8a3f06687b619828f7ece0e86594ea
jurekorbar <korbarjure@gmail.com>
2025-07-12 18:44:51 +0200

    Added lazy importing of subpackages

--- pyfbs/__init__.py
+++ pyfbs/__init__.py
@@ -1,1 +1,31 @@
 __version__ = '1.0.0'
+import importlib as _importlib
+
+submodules = [
+    'data',
+    'display',
+    'dsp',
+    'expansion',
+    'fbs',
+    'interface',
+    'io',
+    'mck',
+    'modal_id',
+    'tpa',
+    'utility',
+]
+
+__all__ = submodules + [
+    '__version__',
+]
+
+
+def __dir__():
+    return __all__
+
+
+def __getattr__(name):
+    if name in submodules:
+        return _importlib.import_module(f'pyfbs.{name}')
+    else:
+        raise AttributeError(f"Module 'pyfbs' has no attribute '{name}'")
